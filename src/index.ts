59e453d406b97440f17c5bd829fc16d9ccb424ab
diego.blom <blomd9@hva.nl>
2024-09-10 13:49:40 +0200

    localstorage added for group name

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("Hello world from TypeScript!");
+const groupName: string = "";
+
+function getTripName() {
+    const inputUitje: HTMLInputElement | null = document.getElementById("uitjeName") as HTMLInputElement | null;
+
+    if (inputUitje !== null) {
+        const tripName: string = inputUitje.value;
+
+        try{
+            localStorage.setItem(groupName, tripName);
+        } catch (e) {
+            console.error("Could not save group name.");
+        }
+        let getGroupName = localStorage.getItem(groupName);
+        console.log(getGroupName);
+    }
+    else {
+        console.error("Geen naam gevonden!");
+    }
+}
+// function testShowLocalStorage() {
+//     let getGroupName = localStorage.getItem(groupName);
+//     console.log(getGroupName);
+// }
