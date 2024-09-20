7481a17ac5b75554747573a0bf6d90ed50c6b0b3
diego.blom <blomd9@hva.nl>
2024-09-20 14:22:26 +0200

    bestaand

--- src/LocalStorage.ts
+++
@@ -1,8 +0,0 @@
-export function saveListToLocalStorage(names: string[]) {
-    localStorage.setItem("peopleList", JSON.stringify(names));
-}
-
-export function loadListFromLocalStorage(): string[] {
-    const storedList = localStorage.getItem("peopleList");
-    return storedList ? JSON.parse(storedList) : [];
-}
